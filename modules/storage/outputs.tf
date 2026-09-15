dd4ae0ab7d502280d5e50329c7cf7fb688d18a63
Zac Israel <zac390@gmail.com>
2026-09-15 09:11:04 -0400

    feat: add DynamoDB processed events storage

--- modules/storage/outputs.tf
+++ modules/storage/outputs.tf
@@ -11,3 +11,17 @@ output "bucket_arn" {
   description = "ARN of the S3 ingestion bucket."
   value       = aws_s3_bucket.ingestion.arn
 }
+
+# Exposes the DynamoDB table name to calling modules and Terragrunt
+# configurations that need to interact with the processed-events table.
+output "dynamodb_table_name" {
+  description = "Name of the DynamoDB table used to store processed events."
+  value       = aws_dynamodb_table.processed_events.name
+}
+
+# Exposes the DynamoDB table ARN for IAM policies and other AWS resources
+# that require permission to access the processed-events table.
+output "dynamodb_table_arn" {
+  description = "ARN of the DynamoDB table used to store processed events."
+  value       = aws_dynamodb_table.processed_events.arn
+}
